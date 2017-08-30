7804643e5e72b113f9f28dca644df7dc6408d61e
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2017-08-29 23:49:40 -0300

    Creating First AR Sample

--- FirstARSample/FirstARSample/ViewController.swift
+++ FirstARSample/FirstARSample/ViewController.swift
@@ -7,17 +7,69 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
+
+struct CameraCoordinates {
+    var x = Float()
+    var y = Float()
+    var z = Float()
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ARsceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        ARsceneView.session.run(configuration)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getCameraCoordinates(sceneView: ARSCNView) -> CameraCoordinates {
+        
+        let cameraTransform = sceneView.session.currentFrame!.camera.transform
+        let cameraCoordinatesMatrix = MDLTransform(matrix: cameraTransform)
+        
+        var cameraCoordinates = CameraCoordinates()
+        cameraCoordinates.x = cameraCoordinatesMatrix.translation.x
+        cameraCoordinates.y = cameraCoordinatesMatrix.translation.y
+        cameraCoordinates.z = cameraCoordinatesMatrix.translation.z
+        
+        return cameraCoordinates
+    }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+    
+    @IBAction func addCube(_ sender: Any) {
+        
+        //Adding related to device original position
+        //let zCoord = randomFloat(min: -2, max: 2)
+        //cubeNode.position = SCNVector3(0,0, zCoord)
+        
+        //Ading related to camera position
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1,
+                                                height: 0.1,
+                                                length: 0.1,
+                                                chamferRadius: 0))
+        let _cameraCoordinates = getCameraCoordinates(sceneView: ARsceneView)
+        
+        cubeNode.position = SCNVector3(_cameraCoordinates.x,
+                                       _cameraCoordinates.y,
+                                       _cameraCoordinates.z)
+        
+        ARsceneView.scene.rootNode.addChildNode(cubeNode)
+        
+    }
+    
+    @IBAction func addCup(_ sender: Any) {
     }
 
 
